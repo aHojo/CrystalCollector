86f3940dd7bf90f4dd44d7ee5b77d60b9faf621f
amh5366 <amh5366@upenn.edu>
2018-06-10 22:26:27 -0400

    added css styling

--- assets/logic.js
+++ assets/logic.js
@@ -69,6 +69,27 @@ function softReset(){
     $('#total-match').html(yourScore);
     gameStart();
 }
+
+function reset(){
+    wins = 0 ;
+    losses = 0;
+    randNum = 0;
+    scoreMatch = 0;
+    yourScore = 0;
+
+    scoreMatch = randGen(19, 121);
+    crystal.ruby.value = randGen(1, 12);
+    crystal.sapphire.value = randGen(1, 13);
+    crystal.emerald.value = randGen(1, 13);
+    crystal.diamond.value = randGen(1, 13);
+
+    console.log(`Ruby ${crystal.ruby.value} Sapphire ${crystal.sapphire.value} Emerald ${crystal.emerald.value} Diamond ${crystal.diamond.value }`)
+    $('#score').html(scoreMatch);
+    $('#total-match').html(yourScore);
+    $('#wins').html(wins);
+    $('#losses').html(losses);
+    
+}
     
 gameStart();
 $("#ruby").on("click", function() {
@@ -83,5 +104,7 @@ $("#emerald").on("click", function() {
 $("#diamond").on("click", function() {
     addCrystals(crystal.diamond.value)
 });
+
+$('#reset').on("click", reset);
     
 
